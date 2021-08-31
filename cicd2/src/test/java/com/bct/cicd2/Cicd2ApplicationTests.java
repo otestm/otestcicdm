43ed63283a0a6693ad006306c11c8a7ab38e2533
unknown <me@gmail.com>
2021-08-31 12:57:31 +0530

    initial

---
+++ cicd2/src/test/java/com/bct/cicd2/Cicd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bct.cicd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
