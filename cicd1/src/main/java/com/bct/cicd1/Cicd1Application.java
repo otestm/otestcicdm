416f95f46cc9edd712db214315aed11142480a2d
otestm <63998753+otestm@users.noreply.github.com>
2021-09-02 23:55:38 +0530

    Update Cicd1Application.java

--- cicd1/src/main/java/com/bct/cicd1/Cicd1Application.java
+++ cicd1/src/main/java/com/bct/cicd1/Cicd1Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Cicd1Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(Cicd1Application.class, args);
 	}
 
